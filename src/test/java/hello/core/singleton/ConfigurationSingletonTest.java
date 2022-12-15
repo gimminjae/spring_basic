1bb09f679a9f92061df820b292e8fe13833abe1e
gimminjae <min356812@naver.com>
2022-12-15 19:37:47 +0900

    @Configuration의 유무에 따른 차이

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -29,4 +29,27 @@ public class ConfigurationSingletonTest {
 
         //테스트 실행시 memberRepository가 3번 호출되어야 할 것 같지만 1번만 호출된다.
     }
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+
+        //순수한 클래스라면 다음과 같이 출력되어야 한다.
+        //class hello.core.AppConfig
+        //그런데 예상과는 다르게 클래스 명에 xxxCGLIB가 붙으면서 상당히 복잡해진 것을 볼 수 있다.
+        //이것은 내가 만든 클래스가 아니라 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서 AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고,
+        //그 다른 클래스를 스프링 빈으로 등록한 것이다!
+        //@Bean이 붙은 메서드마다 이미 스프링 빈이 존재하면 존재하는 빈을 반환하고,
+        //스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환하는 코드가 동적으로 만들어진다.
+        //덕분에 싱글톤이 보장되는 것이다.
+
+        //정리
+        //@Bean만 사용해도 스프링 빈으로 등록되지만, 싱글톤을 보장하지 않는다.
+        //memberRepository() 처럼 의존관계 주입이 필요해서 메서드를 직접 호출할 때 싱글톤을 보장하지 않는다.
+        //크게 고민할 것이 없다. 스프링 설정 정보는 항상 @Configuration 을 사용하자.
+    }
 }
